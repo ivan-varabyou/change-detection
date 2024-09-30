33ca04097e18832ad5d3dd4d76249ff6c7ce84db
Ivan Varabyou <onebelbiz@gmail.com>
2024-09-30 17:56:37 +0300

    feat: remove zone

--- src/app/components/default.component.ts
+++ src/app/components/default.component.ts
@@ -132,91 +132,80 @@ export class DefaultComponent
     this.print('ngAfterViewInit');
     const speed = this.speed;
 
-    this.zone.runOutsideAngular(() => {
-      fromEvent(this.markForCheckButton?.nativeElement, 'click')
-        .pipe()
-        .subscribe(() => {
-          this.markForCheckButton?.nativeElement.classList.add('active');
-          this.cdr.markForCheck();
-        });
+    fromEvent(this.markForCheckButton?.nativeElement, 'click')
+      .pipe()
+      .subscribe(() => {
+        this.markForCheckButton?.nativeElement.classList.add('active');
+        this.cdr.markForCheck();
+      });
 
-      fromEvent(this.detectChangesButton?.nativeElement, 'click')
-        .pipe()
-        .subscribe(() => {
-          this.detectChangesButton?.nativeElement.classList.add('active');
-          this.cdr.detectChanges();
-        });
+    fromEvent(this.detectChangesButton?.nativeElement, 'click')
+      .pipe()
+      .subscribe(() => {
+        this.detectChangesButton?.nativeElement.classList.add('active');
+        this.cdr.detectChanges();
+      });
 
-      fromEvent(this.detachButton?.nativeElement, 'click')
-        .pipe(distinctUntilChanged())
-        .subscribe(() => {
-          this.detachButton?.nativeElement.classList.add('active');
-          this.cdr.detach();
-        });
+    fromEvent(this.detachButton?.nativeElement, 'click')
+      .pipe(distinctUntilChanged())
+      .subscribe(() => {
+        this.detachButton?.nativeElement.classList.add('active');
+        this.cdr.detach();
+      });
 
-      fromEvent(this.reattachButton?.nativeElement, 'click')
-        .pipe(distinctUntilChanged())
-        .subscribe(() => {
-          this.removeStyleTimeout(this.reattachButton?.nativeElement);
-          this.detachButton?.nativeElement.classList.remove('active');
-          this.cdr.reattach();
-        });
+    fromEvent(this.reattachButton?.nativeElement, 'click')
+      .pipe(distinctUntilChanged())
+      .subscribe(() => {
+        this.removeStyleTimeout(this.reattachButton?.nativeElement);
+        this.detachButton?.nativeElement.classList.remove('active');
+        this.cdr.reattach();
+      });
 
-      fromEvent(this.markAsDirtyButton?.nativeElement, 'click')
-        .pipe(distinctUntilChanged())
-        .subscribe(() => {
-          this.value++;
-          this.markAsDirtyButton?.nativeElement.classList.add('dirty');
-        });
+    fromEvent(this.markAsDirtyButton?.nativeElement, 'click')
+      .pipe(distinctUntilChanged())
+      .subscribe(() => {
+        this.value++;
+        this.markAsDirtyButton?.nativeElement.classList.add('dirty');
+      });
 
+    setTimeout(() => {
+      this.afterViewInitLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.afterViewInitLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.afterViewInitLch?.nativeElement.classList.remove('active');
-        }, 6 * this.speed);
+        this.afterViewInitLch?.nativeElement.classList.remove('active');
       }, 6 * this.speed);
-    });
+    }, 6 * this.speed);
   }
 
   public removeStyleTimeout(element: any) {
-    this.zone.runOutsideAngular(() => {
-      element.classList.add('active');
-      setTimeout(() => {
-        element.classList.remove('active');
-      }, 1000);
-    });
+    element.classList.add('active');
+    setTimeout(() => {
+      element.classList.remove('active');
+    }, 1000);
   }
 
-  constructor(
-    private cdr: ChangeDetectorRef,
-    private el: ElementRef,
-    protected zone: NgZone
-  ) {
+  constructor(private cdr: ChangeDetectorRef, private el: ElementRef) {
     this.print('constructor', this.componentName);
     if (!isCdVisualiser) return;
-    this.zone.runOutsideAngular(() => {
+
+    setTimeout(() => {
+      this.constructorLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.constructorLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.constructorLch?.nativeElement.classList.remove('active');
-        }, 0 * this.speed);
+        this.constructorLch?.nativeElement.classList.remove('active');
       }, 0 * this.speed);
-    });
+    }, 0 * this.speed);
   }
 
   public ngOnInit(): void {
     if (!isCdVisualiser) return;
     this.print('ngOnInit');
 
-    this.zone.runOutsideAngular(() => {
+    setTimeout(() => {
+      this.speed = this.speed + this.timeDelay;
+      this.onInitLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.speed = this.speed + this.timeDelay;
-        this.onInitLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.onInitLch?.nativeElement.classList.remove('active');
-        }, 1 * this.speed);
+        this.onInitLch?.nativeElement.classList.remove('active');
       }, 1 * this.speed);
-    });
+    }, 1 * this.speed);
   }
 
   public ngOnChanges(): void {
@@ -224,14 +213,13 @@ export class DefaultComponent
     performance.mark(this.componentName + 'start');
     this.print('ngOnChanges');
     // this.runColorLch(this.onChangesLch?.nativeElement);
-    this.zone.runOutsideAngular(() => {
+
+    setTimeout(() => {
+      this.onChangesLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.onChangesLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.onChangesLch?.nativeElement.classList.remove('active');
-        }, 2 * this.speed);
+        this.onChangesLch?.nativeElement.classList.remove('active');
       }, 2 * this.speed);
-    });
+    }, 2 * this.speed);
   }
 
   public ngDoCheck(): void {
@@ -240,95 +228,86 @@ export class DefaultComponent
     this.print('ngDoCheck');
     this.timeDelay = 0;
 
-    this.zone.runOutsideAngular(() => {
+    setTimeout(() => {
+      this.doCheckLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.doCheckLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.doCheckLch?.nativeElement.classList.remove('active');
-        }, 3 * this.speed);
+        this.doCheckLch?.nativeElement.classList.remove('active');
       }, 3 * this.speed);
-    });
+    }, 3 * this.speed);
   }
 
   public ngAfterContentInit(): void {
     if (!isCdVisualiser) return;
     this.print('ngAfterContentInit');
-    this.zone.runOutsideAngular(() => {
+
+    setTimeout(() => {
+      this.afterContentInitLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.afterContentInitLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.afterContentInitLch?.nativeElement.classList.remove('active');
-        }, 4 * this.speed);
+        this.afterContentInitLch?.nativeElement.classList.remove('active');
       }, 4 * this.speed);
-    });
+    }, 4 * this.speed);
   }
 
   public ngAfterContentChecked(): void {
     if (!isCdVisualiser) return;
     this.print('ngAfterContentChecked');
 
-    this.zone.runOutsideAngular(() => {
+    setTimeout(() => {
+      this.afterContentCheckedLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.afterContentCheckedLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.afterContentCheckedLch?.nativeElement.classList.remove('active');
-        }, 5 * this.speed);
+        this.afterContentCheckedLch?.nativeElement.classList.remove('active');
       }, 5 * this.speed);
-    });
+    }, 5 * this.speed);
   }
 
   public triggerChangeDetection() {
     if (!isCdVisualiser) return;
-    this.zone.runOutsideAngular(() => {
-      this.print('template');
-      this.zone.runOutsideAngular(() => {
-        performance.mark(this.componentName + 'end');
 
-        performance.measure(
-          `${this.componentName}start to ${this.componentName}end`,
-          this.componentName + 'start',
-          this.componentName + 'end'
-        );
-        const [measure] = performance.getEntriesByName(
-          `${this.componentName}start to ${this.componentName}end`
-        );
-        this.timeDelay = measure.duration;
-        this.speed = this.speed + this.timeDelay;
+    this.print('template');
 
-        setTimeout(() => {
-          // this.print('changeDetectionTrigger');
-          const nodeElement = this.el.nativeElement.querySelector('.name');
-          nodeElement?.classList.add('checked');
-          this.timeout = setTimeout(() => {
-            nodeElement?.classList.remove('checked');
-            const dirtyNumber =
-              +this.markAsDirtyButton?.nativeElement.textContent
-                .replace('(', '')
-                .replace(')', '')
-                .split(' ++ ')[1];
-            if (dirtyNumber === this.value) {
-              this.markAsDirtyButton?.nativeElement.classList.remove('dirty');
-            }
-            this.markForCheckButton?.nativeElement.classList.remove('active');
-            this.detectChangesButton?.nativeElement.classList.remove('active');
-            clearTimeout(this.timeout);
-          }, 5 * this.speed);
-        }, 5 * this.speed);
-      });
-    });
+    performance.mark(this.componentName + 'end');
+
+    performance.measure(
+      `${this.componentName}start to ${this.componentName}end`,
+      this.componentName + 'start',
+      this.componentName + 'end'
+    );
+    const [measure] = performance.getEntriesByName(
+      `${this.componentName}start to ${this.componentName}end`
+    );
+    this.timeDelay = measure.duration;
+    this.speed = this.speed + this.timeDelay;
+
+    setTimeout(() => {
+      // this.print('changeDetectionTrigger');
+      const nodeElement = this.el.nativeElement.querySelector('.name');
+      nodeElement?.classList.add('checked');
+      this.timeout = setTimeout(() => {
+        nodeElement?.classList.remove('checked');
+        const dirtyNumber = +this.markAsDirtyButton?.nativeElement.textContent
+          .replace('(', '')
+          .replace(')', '')
+          .split(' ++ ')[1];
+        if (dirtyNumber === this.value) {
+          this.markAsDirtyButton?.nativeElement.classList.remove('dirty');
+        }
+        this.markForCheckButton?.nativeElement.classList.remove('active');
+        this.detectChangesButton?.nativeElement.classList.remove('active');
+        clearTimeout(this.timeout);
+      }, 5 * this.speed);
+    }, 5 * this.speed);
   }
 
   public ngAfterViewChecked(): void {
     if (!isCdVisualiser) return;
     this.print('ngAfterViewChecked');
-    this.zone.runOutsideAngular(() => {
+
+    setTimeout(() => {
+      this.afterViewCheckedLch?.nativeElement.classList.add('active');
       setTimeout(() => {
-        this.afterViewCheckedLch?.nativeElement.classList.add('active');
-        setTimeout(() => {
-          this.afterViewCheckedLch?.nativeElement.classList.remove('active');
-        }, 7 * this.speed);
+        this.afterViewCheckedLch?.nativeElement.classList.remove('active');
       }, 7 * this.speed);
-    });
+    }, 7 * this.speed);
   }
 
   public ngOnDestroy(): void {
