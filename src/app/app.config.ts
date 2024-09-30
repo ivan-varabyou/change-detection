0d85b722fd788cd9d754d64129913e238fa4a4ee
Ivan Varabyou <onebelbiz@gmail.com>
2024-09-30 16:12:50 +0300

    fix: appConfig providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,17 +7,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-const isZonneless = false;
-let providers: any[];
-if (isZonneless) {
-  providers = [provideExperimentalZonelessChangeDetection()];
-} else {
-  providers = [
+export const appConfig: ApplicationConfig = {
+  providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-  ];
-}
-
-export const appConfig: ApplicationConfig = {
-  providers,
+  ],
 };
