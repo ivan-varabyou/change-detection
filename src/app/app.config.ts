15db7b894e39339b9aa3a725af12008c2e65302c
Ivan Varabyou <onebelbiz@gmail.com>
2024-09-30 16:08:38 +0300

    feat: add two way zone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+const isZonneless = false;
+let providers: any[];
+if (isZonneless) {
+  providers = [provideExperimentalZonelessChangeDetection()];
+} else {
+  providers = [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ];
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers,
 };
